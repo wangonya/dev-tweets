bd6331ca8e6da86510487843e27cf6cf89f4781f
wangonya <kwangonya@gmail.com>
2019-09-26 17:18:59 +0300

    add utils

---
+++ src/utils.js
@@ -0,0 +1,16 @@
+export const apiUrl = "http://127.0.0.1:5000";
+export const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
+];
+export const date = new Date();
